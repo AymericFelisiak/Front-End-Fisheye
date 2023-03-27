de0f79b1867fa8a45e070f72fb79202d36ac5920
AymericFelisiak <118811300+AymericFelisiak@users.noreply.github.com>
2023-03-27 23:08:06 +0200

    Github pages tests

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,4 @@
-import {PhotographerFactory} from "/scripts/factories/photographer.js";
+import {PhotographerFactory} from "../factories/photographer.js";
 
 let activeIndex = 0;
 const firstFocusable = document.querySelector('#home');
